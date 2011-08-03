7fc29f1ba08a5a44ef66528a6209655f53ce5a62
Adron Hall <adronhall@gmail.com>
2011-08-03 16:12:38 -0700

    Adding first solution against Quartz libraries.

---
+++ quartz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Quartz;
+using Quartz.Impl;
+
+namespace quartz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            Console.WriteLine("started");
+
+            JobDetail jobDetail = new JobDetail("myJob", null, typeof(HelloJob));
+            Trigger trigger = TriggerUtils.MakeSecondlyTrigger();
+           
+            trigger.StartTimeUtc = TriggerUtils.GetEvenHourDate(DateTime.UtcNow);
+            trigger.Name = "myTrigger";
+            sched.ScheduleJob(jobDetail, trigger);
+            sched.Start();
+
+            Console.WriteLine("started again");
+        }
+    }
+
+    internal class HelloJob : IJob
+    {
+        public void Execute(JobExecutionContext context)
+        {
+            Console.WriteLine("Doing something now: {0}", DateTime.Now);
+        }
+    }
+}
