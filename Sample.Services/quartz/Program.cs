6f4548758cefa97e32d3a01fac1567e61d21d38a
Adron Hall <adronhall@gmail.com>
2011-08-03 23:20:30 -0700

    Arranged files and folders to match the previous project solution. Also added images for the project.

---
+++ Sample.Services/quartz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Quartz;
+using Quartz.Impl;
+
+namespace quartz
+{
+    class Program
+    {
+        static void Main()
+        {
+            // construct a scheduler factory
+            var schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            // construct job info
+            var jobDetail = new JobDetail("myJob", null, typeof(DumbJob));
+            // fire every 5 seconds.
+            var trigger = TriggerUtils.MakeSecondlyTrigger(5);
+            // start on the next even hour
+            trigger.StartTimeUtc = TriggerUtils.GetEvenHourDate(DateTime.UtcNow);
+            trigger.Name = "myTrigger";
+            sched.ScheduleJob(jobDetail, trigger); 
+        }
+    }
+
+    public class DumbJob : IJob
+    {
+        public DumbJob()
+        {
+        }
+
+        public void Execute(JobExecutionContext context)
+        {
+            Console.WriteLine("DumbJob is executing.");
+        }
+    } 
+
+}
